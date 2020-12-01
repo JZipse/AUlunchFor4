5c686dc2d77637853c8e23eb855ac132827d6e0d
bjaynes01 <55394496+bjaynes01@users.noreply.github.com>
2020-11-30 18:39:11 -0600

    Update server.js

--- back_end/server.js
+++ back_end/server.js
@@ -10,7 +10,7 @@ const con = mysql.createConnection({
     database: "lunch44F2020"
 });
 
-+con.connect(function(err) {
+con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
 });
