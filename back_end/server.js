41ab39091d00894bb5d13013697ba7ca017964e0
bakers99 <bryanakers1999@gmail.com>
2020-12-04 12:56:34 -0600

    Update server.js

--- back_end/server.js
+++ back_end/server.js
@@ -10,7 +10,8 @@ const flash = require('express-flash')
 const session = require('express-session');
 const initializePassport = require('./passport-config');
 const { body, validationResult } = require('express-validator');
-const methodOverride = require('method-override')
+const methodOverride = require('method-override');
+const { count } = require('console');
 
 
 initializePassport(
@@ -52,36 +53,36 @@ app.use(methodOverride('_method'))
 
 
 // forgot password functionalsity.
-app.get('/update/password', (req, res) =>{
+app.get('/update/password', (req, res) => {
     res.render('PasswordUpdate')
 })
 
-app.post('/update/password/action',[
+app.post('/update/password/action', [
     body("email").notEmpty().isEmail().withMessage("Input Email for Updating your Password."),
-    body("password").notEmpty().isLength({min: 4, max: 14}).withMessage("Input a new Pass word that is 4-14 digits long")
-], (req, res) =>{
+    body("password").notEmpty().isLength({ min: 4, max: 14 }).withMessage("Input a new Pass word that is 4-14 digits long")
+], (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        for(let i = 0;i < errors.array().length;i++){
+        for (let i = 0; i < errors.array().length; i++) {
             console.log("test: ", "param: " + errors.array()[i].param + "msg: " + errors.array()[i].msg)
             req.flash(errors.array()[i].param, errors.array()[i].msg)
         }
         res.redirect('/update/password')
-    }else{
+    } else {
         console.log('Got body:', req.body)
-        con.query('SELECT EMAIL FROM users', async (err,rows) => {
+        con.query('SELECT EMAIL FROM users', async (err, rows) => {
             console.log('data: ', rows)
             var pass;
 
-            if(err) throw err;
-            for(let i = 0; i < rows.length; i++){
-                if(rows[i].EMAIL == req.body.email){
+            if (err) throw err;
+            for (let i = 0; i < rows.length; i++) {
+                if (rows[i].EMAIL == req.body.email) {
                     pass = req.body.password;
                 }
             }
 
             console.log("pass: ", pass)
-            if(pass != null){
+            if (pass != null) {
                 // having trouble getting this to work.
                 console.log('Got body:', pass)
                 const hashedPassword = await bcrypt.hash(pass, 10);
@@ -90,7 +91,7 @@ app.post('/update/password/action',[
                 con.query(str)
                 res.redirect('/adminLogin')
                 //customers.length = 0;
-            }else{
+            } else {
                 console.log(pass != null)
                 req.flash('noAccount', 'No Account has been made for that Email.')
                 res.redirect('/update/password')
@@ -100,34 +101,19 @@ app.post('/update/password/action',[
 })
 
 // admin functionality begins here.
-app.get('/form', (req, res) =>{
+app.get('/form', (req, res) => {
     res.render('form');
 });
 
-app.post('/formaction', [
-                        body("fName").notEmpty().withMessage("Must have a First Name."), 
-                        body("lName").notEmpty().withMessage("Must have a Last Name."), 
-                        body("ID").notEmpty().isLength({ min: 7, max: 7 }).withMessage("Your school's ID's are 7 digits Long."), 
-                        body("Email").notEmpty().isEmail().withMessage("You must input a proper email address."), 
-                        body("password").notEmpty().isLength({ min: 4, max: 14 }).withMessage("You must have password that is 4-14 characters in length."), body("Role").notEmpty().withMessage("you must have a role."), 
-                        body("Dept").notEmpty().withMessage("You must have a department.")
-                        ], async (req,res) =>{
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        for(let i = 0;i < errors.array().length;i++){
-            console.log("test: ", "param: " + errors.array()[i].param + "msg: " + errors.array()[i].msg)
-            req.flash(errors.array()[i].param, errors.array()[i].msg)
-        }
-        res.redirect('/form')
-    }else{
-        console.log('Got body:', req.body)
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+app.post('/formaction', async (req, res) => {
 
-        var str = "INSERT INTO `users` (`firstName`, `lastName`, `schoolID`, `email`, `password`, `staffRole`, `department`, `active`) VALUES ('" + req.body.fName + "', '" + req.body.lName + "', '" + req.body.ID + "', '" + req.body.Email + "', '" + hashedPassword + "', '" + req.body.Role + "', '" + req.body.Dept + "', '1')";
-        console.log(str);
-        con.query(str);
-        res.redirect('/adminLogin')
-    }
+    console.log('Got body:', req.body)
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    var str = "INSERT INTO `users` (`firstName`, `lastName`, `schoolID`, `email`, `password`, `staffRole`, `department`, `active`) VALUES ('" + req.body.fName + "', '" + req.body.lName + "', '" + req.body.ID + "', '" + req.body.Email + "', '" + hashedPassword + "', '" + req.body.Role + "', '" + req.body.Dept + "', '1')";
+    console.log(str);
+    con.query(str);
+    res.redirect('/adminLogin')
 })
 
 app.get('/form/delete', (req, res) => {
@@ -142,18 +128,18 @@ app.get('/form/delete', (req, res) => {
     });
 })
 
-app.post('/form/delete/action',[
+app.post('/form/delete/action', [
     body("ID").notEmpty().isNumeric().withMessage("something went wrong with the application ID was not found.")
 
-], (req,res) => {
+], (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        for(let i = 0;i < errors.array().length;i++){
+        for (let i = 0; i < errors.array().length; i++) {
             console.log("test: ", "param: " + errors.array()[i].param + "msg: " + errors.array()[i].msg)
             req.flash(errors.array()[i].param, errors.array()[i].msg)
         }
         res.redirect('/form/delete')
-    }else{
+    } else {
         console.log('Got body:', req.body)
         var str = "DELETE FROM `users` WHERE (`internalID` = '" + req.body.ID + "')";
         console.log(str);
@@ -172,64 +158,65 @@ app.get('/generateMeetings', checkAuthenticated, (req, res) => {
 
 app.post('/newMeeting', async (req, res) => {
     var meeting = [];
-    con.query("SELECT count(*) AS count FROM lunch44F2020.users WHERE active = 1;", function (err, results) {
-        var remain = results[0].count % 4;
+    con.query("SELECT count(*) AS count FROM lunch44F2020.users WHERE active = 1;", function (err, results1) {
+        var remain = results1[0].count % 4;
         var loops;
-        if (results[0].count < 3) {
-            console.log("Not Enough Users For Meeting")
-        } else if (results[0].count == 6) {
-            loops = 3;
-        } else {
-            if (remain == 1) {
-                loops = 5;
-            } else if (remain == 2) {
-                loops = 4;
-            } else if (remain == 3) {
-                if (results[0].count > 3) {
-                    loops = 4;
-                } else {
-                    loops = 3;
-                }
+        if (count > 3) {
+            if (results1[0].count == 6) {
+                loops = 3;
             } else {
-                loops = 4;
+                if (remain == 1) {
+                    loops = 5;
+                } else if (remain == 2) {
+                    loops = 4;
+                } else if (remain == 3) {
+                    if (results1[0].count > 3) {
+                        loops = 4;
+                    } else {
+                        loops = 3;
+                    }
+                } else {
+                    loops = 4;
+                }
             }
+
+            con.query("SELECT internalID FROM users WHERE active = 1 ORDER BY RAND() LIMIT " + loops + ";", function (err, result, fields) {
+                if (err) throw err;
+                for (i = 0; i < loops; i++) {
+                    console.log(result[i].internalID);
+                    meeting.push(result[i].internalID);
+                    con.query("UPDATE users SET active = 0 WHERE (`internalID` = '" + result[i].internalID + "')");
+                }
+                console.log(meeting);
+                if (loops == 5) {
+                    con.query("INSERT INTO `meetings`(`meetDate`, `member1`, `member2`, `member3`, `member4`, `member5`, `meetingLeader`) VALUES('" + null + "', '" + meeting[0] + "', '" + meeting[1] + "', '" + meeting[2] + "', '" + meeting[3] + "','" + meeting[4] + "','" + null + "' )");
+                } else if (loops == 4) {
+                    con.query("INSERT INTO `meetings`(`meetDate`, `member1`, `member2`, `member3`, `member4`, `member5`, `meetingLeader`) VALUES('" + null + "', '" + meeting[0] + "', '" + meeting[1] + "', '" + meeting[2] + "', '" + meeting[3] + "','" + null + "','" + null + "' )");
+                } else {
+                    con.query("INSERT INTO `meetings`(`meetDate`, `member1`, `member2`, `member3`, `member4`, `member5`, `meetingLeader`) VALUES('" + null + "', '" + meeting[0] + "', '" + meeting[1] + "', '" + meeting[2] + "', '" + null + "','" + null + "','" + null + "' )");
+                }
+
+            });
+
+        } else {
+            console.log("Not Enough Users For Meeting")
+            res.redirect('/generateMeetings')
+            res.end()
         }
-
-    console.log("Loop:" + loops);
-    console.log(remain);
-    console.log(results[0].count);
-    con.query("SELECT internalID FROM users WHERE active = 1 ORDER BY RAND() LIMIT 4;", function (err, result, fields) {
-        if (err) throw err;
-        console.log(results);
     });
-    //  con.query("SELECT internalID FROM users WHERE active = 1 ORDER BY RAND() LIMIT 4;", function (err, result, fields) {
-    //      if (err) throw err;
-    //     console.log(result[0].internalID);
-    //     //  meeting.push(result[0].internalID);
-    //     //  meeting.push(result[1].internalID);
-    //     //  meeting.push(result[2].internalID);
-    //     //  meeting.push(result[3].internalID);
-    //     //  for (i = 0; i <= 3; i++) {
-    //     //      con.query("UPDATE users SET active = 0 WHERE (`internalID` = '" + result[i].internalID + "')");
-    //     //  }
-
-    // //     con.query("INSERT INTO `meetings`(`meetDate`, `member1`, `member2`, `member3`, `member4`) VALUES('" + null + "', '" + meeting[0] + "', '" + meeting[1] + "', '" + meeting[2] + "', '" + meeting[3] + "')");
-    // // });
-    res.redirect('/generateMeetings')
-});
 });
 
 app.get('/adminLogin', checkNotAuthenticated, (req, res) => {
     customers.length = 0;
-    con.query('SELECT internalID, EMAIL, PASSWORD, ISADMIN FROM users', (err,rows) => {
-        if(err) throw err;
+    con.query('SELECT internalID, EMAIL, PASSWORD, ISADMIN FROM users', (err, rows) => {
+        if (err) throw err;
         console.log('Data received from Db:');
-        for(let i = 0; i < (rows.length); i++){
-            const user = {email: rows[i].EMAIL, password: rows[i].PASSWORD, id: rows[i].internalID, admin: rows[i].ISADMIN};
-            customers.push(user); 
+        for (let i = 0; i < (rows.length); i++) {
+            const user = { email: rows[i].EMAIL, password: rows[i].PASSWORD, id: rows[i].internalID, admin: rows[i].ISADMIN };
+            customers.push(user);
         }
     });
-    setTimeout(function(){res.render('adminLogin')}, 100);
+    setTimeout(function () { res.render('adminLogin') }, 100);
 });
 
 app.get('/adminPage', checkAuthenticated, checkRole(1), (req, res) => {
@@ -251,41 +238,41 @@ app.post('/customerLogin', passport.authenticate('local', {
 }));
 
 
-app.post('/customer/delete', (req,res) => {
-        console.log('Got body:', req.user)
-        var str = "DELETE FROM `users` WHERE (`internalID` = '" + req.user.id + "')";
-        console.log(str);
-        con.query(str);
-        
-        
+app.post('/customer/delete', (req, res) => {
+    console.log('Got body:', req.user)
+    var str = "DELETE FROM `users` WHERE (`internalID` = '" + req.user.id + "')";
+    console.log(str);
+    con.query(str);
+
+
 })
 
-app.get('/customer/update', (req,res) =>{
-        console.log('Got user:', req.user)
-        con.query('SELECT * FROM users WHERE internalID = ' + req.user.id, (err,rows) => {
-            console.log('Data received from Db:');
-            console.log(rows);
-            res.render('customerUpdate', {"data": rows})
-        })
+app.get('/customer/update', (req, res) => {
+    console.log('Got user:', req.user)
+    con.query('SELECT * FROM users WHERE internalID = ' + req.user.id, (err, rows) => {
+        console.log('Data received from Db:');
+        console.log(rows);
+        res.render('customerUpdate', { "data": rows })
+    })
 })
 
-app.post('/customer/update/action',[
+app.post('/customer/update/action', [
     body("fName").notEmpty().withMessage("You must input a First Name."),
     body("lName").notEmpty().withMessage("You must have a Last Name."),
-    body("schoolID").notEmpty().isNumeric().isLength({min:7, max:7}).withMessage("School IDs are 7 digits long."),
+    body("schoolID").notEmpty().isNumeric().isLength({ min: 7, max: 7 }).withMessage("School IDs are 7 digits long."),
     body("Role").notEmpty().withMessage("You must have a Role Selected."),
     body("Dept").notEmpty().withMessage("You must have a Department Selected.")
-], (req,res) =>{
+], (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        for(let i = 0;i < errors.array().length;i++){
+        for (let i = 0; i < errors.array().length; i++) {
             console.log("test: ", "param: " + errors.array()[i].param + "msg: " + errors.array()[i].msg)
             req.flash(errors.array()[i].param, errors.array()[i].msg)
         }
         res.redirect('/customer/update')
-    }else{
+    } else {
         console.log('Got user:', req.user)
-        var str = "UPDATE `users` SET `firstName` = '" + req.body.fName + "', `lastName` = '" + req.body.lName + "', `schoolID` = '" + req.body.schoolID + "', `staffRole` = '" + req.body.Role + "', `department` = '"+ req.body.Dept +"', `active` = '1' WHERE (`internalID` = '" + req.body.ID + "')";
+        var str = "UPDATE `users` SET `firstName` = '" + req.body.fName + "', `lastName` = '" + req.body.lName + "', `schoolID` = '" + req.body.schoolID + "', `staffRole` = '" + req.body.Role + "', `department` = '" + req.body.Dept + "', `active` = '1' WHERE (`internalID` = '" + req.body.ID + "')";
         con.query(str);
         res.redirect("/customerPage");
     }
@@ -295,7 +282,7 @@ app.get('/feedback', checkAuthenticated, (req, res) => {
     res.render('feedback');
 });
 
-app.post('/feedback/Insert',[
+app.post('/feedback/Insert', [
     body("feedback").notEmpty().withMessage("Feedback on your meeting would be very helpful.")
 ], (req, res) => {
     const errors = validationResult(req);
@@ -303,34 +290,34 @@ app.post('/feedback/Insert',[
         console.log("this is interesting: ", errors.array()[0])
         req.flash('err', errors.array()[0].msg)
         res.redirect('/feedback')
-    }else{
+    } else {
         console.log('Got body:', req.user)
         res.send("This forms functinality has yet to be implemented.")
     }
 });
 
-app.get('/inactiveToggle', (req, res) =>{
-    con.query('SELECT * FROM users', (err,rows) => {
-        if(err) throw err;
+app.get('/inactiveToggle', (req, res) => {
+    con.query('SELECT * FROM users', (err, rows) => {
+        if (err) throw err;
         var act = -1;
-        for(let i = 0; i < rows.length; i++){
-            if(rows[i].internalID == req.user.id){
-                if(Number(rows[i].active) == 1){
+        for (let i = 0; i < rows.length; i++) {
+            if (rows[i].internalID == req.user.id) {
+                if (Number(rows[i].active) == 1) {
                     console.log(Number(rows[i].active))
                     act = 0;
-                }else{
+                } else {
                     console.log(Number(rows[i].active))
                     act = 1;
                 }
             }
         }
         console.log('act:', act)
-        if (act === 1){
+        if (act === 1) {
             req.flash('active', 'you now are active')
             var str = "UPDATE `users` SET `active` = '" + act + "' WHERE (`internalID` = '" + req.user.id + "')";
             con.query(str);
             res.redirect('/customerPage')
-        }else{
+        } else {
             req.flash('inactive', 'you now are inactive')
             var str = "UPDATE `users` SET `active` = '" + act + "' WHERE (`internalID` = '" + req.user.id + "')";
             con.query(str);
@@ -339,16 +326,16 @@ app.get('/inactiveToggle', (req, res) =>{
     })
 })
 
-app.get('/customerPage', checkAuthenticated, (req, res) =>{
+app.get('/customerPage', checkAuthenticated, (req, res) => {
     res.render('customerPage');
 });
 
 app.post('/meetingHistory', (req, res) => {
-    con.query(`SELECT * from meetings where  member1 = ${req.user.id} or member2 = ${req.user.id} or member3 = ${req.user.id} or member4 = ${req.user.id} or member5 = ${req.user.id};`, 
-    (err, rows) => {
-        if (err) throw err;
-        res.render('meetingHistory', {meetings: rows});
-    });
+    con.query(`SELECT * from meetings where  member1 = ${req.user.id} or member2 = ${req.user.id} or member3 = ${req.user.id} or member4 = ${req.user.id} or member5 = ${req.user.id};`,
+        (err, rows) => {
+            if (err) throw err;
+            res.render('meetingHistory', { meetings: rows });
+        });
 })
 
 app.delete('/logout', (req, res) => {
@@ -356,23 +343,23 @@ app.delete('/logout', (req, res) => {
     res.redirect('/adminLogin');
 });
 
-function checkAuthenticated(req, res, next){
-    if (req.isAuthenticated()){
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
         return next();
     }
     res.redirect('/adminLogin')
 }
 
-function checkNotAuthenticated(req, res, next){
-    if (req.isAuthenticated()){
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
         return res.redirect('/customerPage')
     }
     next()
 }
 
-function checkRole(role){
-    return ( req, res, next) => {
-        if (req.user.admin !== role){
+function checkRole(role) {
+    return (req, res, next) => {
+        if (req.user.admin !== role) {
             return res.redirect('/adminLogin')
         }
         next()
